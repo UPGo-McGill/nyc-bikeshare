f0250c75aa8f9a0f586b6d772657dc249742fb53
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-08 17:40:31 -0400

    Nearly working?

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -14,31 +14,50 @@ st_erase <- function(x, y) st_difference(x, st_union(st_combine(y)))
 
 ## st_intersect_summarize helper function
 
-st_intersect_summarize <- function(x, y, population, sum_vars, mean_vars,
-                                   precision) {
+st_intersect_summarize <- function(x, y, population, sum_vars, mean_vars) {
   
   population <- enquo(population)
+  population_int <- paste0(quo_name(population),"_int")
   
   intersects <- suppressWarnings(st_intersection(x, y))
-  
   cols <- length(intersects)
   
   intersects <- 
     intersects %>% 
+    mutate(
+      !! population_int := !! population * st_area(.data$geometry) /
+        .data$CT_area
+    ) %>%
     mutate_at(sum_vars, list(`int` =  ~{
           . * st_area(.data$geometry) / .data$CT_area
           }))
 
-  intersects %>% 
-    summarize_at(sum_vars, list())
+  sums <- intersects %>% 
+    summarize_at(c((cols):(cols + length(sum_vars) - 1)), sum, na.rm = TRUE)
   
+  population_int <- enquo(population_int)
+  
+  means <- intersects %>% 
+    #summarize(sum = sum(!! population))
+    summarize_at(mean_vars, list(~{
+     sum(., na.rm = TRUE)
+      #sum((. * (!! population_int) / (!! population)), na.rm = TRUE)
+    }))
+  
+  st_join(sums, means)
 }
 
 
-st_intersect_summarize(CTs, buffer_union, vars(pop_total, pop_white, immigrant, education))
+st_intersect_summarize(
+  CTs,
+  buffer_union,
+  pop_total,
+  vars(pop_white, immigrant, education),
+  vars(med_income)
+  )
 
 
-
+st_intersection(CTs, buffer_union)
 
 
 
