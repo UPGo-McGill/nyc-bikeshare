067a4faa943644952e17a56a27cb1a7a146ab6b6
David Wachsmuth <david.wachsmuth@mcgill.ca>
2019-05-10 10:15:47 -0400

    Tweaked equity comparisons, and removed buffer_union

--- R/equity_comparisons.R
+++ R/equity_comparisons.R
@@ -1,35 +1,14 @@
 ## Equity Comparisons
 
-
-bike_service_comparison <- 
-  bike_service_areas %>% 
-  filter(year == 2018)
-
-subway_service_comparison <- 
-  bike_service_areas %>% 
-  filter(year == 2018)
+bike_service_added <- st_intersection(filter(bike_service_areas, year == 2018),
+                                      subway_service_areas) %>% 
+  filter(bike_service == TRUE)
 
 bike_comparison2018 <- st_intersect_summarize(
   CTs,
-  bike_service_comparison,
+  bike_service_added,
   ID_vars = vars(year, bike_service),
   population = pop_total,
   sum_vars = vars(pop_white, immigrant, education),
   mean_vars = vars(med_income)
 )
-
-subway_comparison2018 <- st_intersect_summarize(
-  CTs,
-  subway_service_comparison,
-  ID_vars = vars(year, bike_service),
-  population = pop_total,
-  sum_vars = vars(pop_white, immigrant, education),
-  mean_vars = vars(med_income)
-)
-
-st_intersection(bike_comparison2018, subway_comparison2018)
-
-
-
-
-                                                                                                       
