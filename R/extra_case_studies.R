74ef0a7845408b0d24ca83c27299ce6c0f6556dc
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-20 23:47:30 -0400

    Bronx map

--- R/extra_case_studies.R
+++ R/extra_case_studies.R
@@ -95,8 +95,95 @@ jhf_subway_catchment_population <-
 
 
 
+### REPEAT FOR SOUTH BRONX
 
-## Need to repeat for South Bronx
+## Get South Bronx streets
+
+bronx_osm <- 
+  target_neighbourhoods %>% 
+  filter(nbhd == "South Bronx") %>% 
+  st_transform(4326) %>% 
+  st_bbox() %>% 
+  bb(ext = 1.2) %>% 
+  as.vector() %>%
+  opq() %>% 
+  add_osm_feature(key = "highway") %>% 
+  osmdata_sf()
+
+bronx_streets <- 
+  rbind(bronx_osm$osm_polygons %>% st_cast("LINESTRING"),
+        bronx_osm$osm_lines) %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_transform(26918) %>%
+  select(osm_id, name, geometry)
+
+bronx_dodgr <- 
+  dodgr_streetnet("bronx new york city") %>% 
+  weight_streetnet() %>% 
+  dodgr_to_sf() %>% 
+  st_sf() %>%
+  as_tibble() %>%
+  st_as_sf() %>%
+  st_transform(26918)
+
+bronx_dodgr <- 
+  target_neighbourhoods %>% 
+  filter(nbhd == "South Bronx") %>% 
+  st_buffer(2500) %>%
+  st_intersection(bronx_dodgr, .)
+
+
+## Develop South Bronx network catchment areas
+
+bronx_stations <- subway_stations_vulnerability[
+  lengths(st_intersects(subway_stations_vulnerability,
+                        target_neighbourhoods %>% 
+                          filter(nbhd == "South Bronx"))) > 0,]
+
+bronx_network <- 
+  bronx_dodgr %>% 
+  as("Spatial") %>% 
+  SpatialLinesNetwork()
+
+bronx_bike_catchment <- 
+  calc_network_catchment(
+    bronx_network, network_city, as(bronx_stations, "Spatial"), c("data"),
+    distance = 25, maximpedance = 2400, dissolve = TRUE) %>% 
+  st_as_sf() %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_union()
+
+bronx_subway_catchment <- 
+  calc_network_catchment(
+    bronx_network, network_city, as(bronx_stations, "Spatial"), c("data"),
+    distance = 25, maximpedance = 960, dissolve = TRUE) %>% 
+  st_as_sf() %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_union()
+
+
+## Versions of catchment areas to use for service population estimates
+
+jhf_bike_catchment_population <- 
+  calc_network_catchment(
+    jhf_network, network_city, as(jhf_stations, "Spatial"), c("data"),
+    distance = 100, maximpedance = 2400, dissolve = TRUE) %>% 
+  st_as_sf() %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_union()
+
+jhf_subway_catchment_population <- 
+  calc_network_catchment(
+    jhf_network, network_city, as(jhf_stations, "Spatial"), c("data"),
+    distance = 100, maximpedance = 960, dissolve = TRUE) %>% 
+  st_as_sf() %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_union()
 
 
 
