216a239af63a8d0c09bd60a17e061fec6e77aacf
louseltz <lou.seltz@mail,mcgill.ca>
2019-05-21 11:17:45 -0400

    includes packages needed for 05_case_studies

--- R/01_helper_functions.R
+++ R/01_helper_functions.R
@@ -13,6 +13,7 @@ library(tmap)
 library(tmaptools)
 library(units)
 library(dodgr)
+library(stplanr)
 
 options(tigris_use_cache = TRUE)
 suppressWarnings(font_import(paths = "data/fonts", prompt = FALSE))
