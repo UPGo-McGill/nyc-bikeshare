ef6bdb23c5076e1f3e39f9a93f4fa74eb96dfb8f
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-24 10:12:32 -0400

    More facts

--- R/07_tables.R
+++ R/07_tables.R
@@ -1,31 +1,86 @@
 ### TABLES AND CHARTS ##########################################################
 
+## NYC summary demographics
+nyc_demographics <- get_acs(
+  geography = "county", 
+  variables = c(pop_non_hisp_white = "B03002_003",
+                pop_non_hisp_black = "B03002_004",
+                pop_hisp = "B03002_012",
+                education = "B16010_041",
+                poverty = "B17001_002"),
+  year = 2017, 
+  state = "36",
+  county = c("New York County",
+             "Kings County",
+             "Queens County",
+             "Bronx County",
+             "Richmond County"),
+  summary_var = "B01003_001") %>% 
+  set_names(c("GEOID", "NAME", "Variable", "Estimate", "MOE",
+              "pop_total", "pop_total_MOE")) %>%
+  select(-MOE, -pop_total_MOE, -GEOID, -NAME) %>% 
+  spread(key = Variable, value = Estimate) %>%
+  filter(pop_total > 100) %>%
+  na.omit() %>% 
+  summarize_all(sum)
+
+
+
 ### EXECUTIVE SUMMARY ####
 
 ## p. 2
 
-# All facts taken from chapter 3
+# All facts taken from:
+
+bike_service_comparison %>% st_drop_geometry()
 
 ## p. 3
 
-# White and non-white bike share percentages
+# While 51.8% of people within Citi Bike’s service area are non-hispanic white,
+# only 26.2% outside of the service area are white. Only 16.5% of people of
+# color in New York City have access to bike sharing services, while 37.5% of
+# white New Yorkers do.
 
-white_bike_share <-
-  (st_drop_geometry(bike_service_comparison)[4,3] *
-     st_drop_geometry(bike_service_comparison)[4,4]) /
-     sum(nyc_demographics$pop_non_hisp_white)
+# White bike share
+(st_drop_geometry(bike_service_comparison)[4,3] *
+    st_drop_geometry(bike_service_comparison)[4,4]) /
+    nyc_demographics$pop_non_hisp_white
 
-non_white_bike_share <-
-  (st_drop_geometry(bike_service_comparison)[4,3] *
-     (1 - st_drop_geometry(bike_service_comparison)[4,4])) / 
-     (sum(nyc_demographics$pop_total) - sum(nyc_demographics$pop_non_hisp_white))
+# Non white bike share
+(st_drop_geometry(bike_service_comparison)[4,3] *
+    (1 - st_drop_geometry(bike_service_comparison)[4,4])) / 
+    (sum(nyc_demographics$pop_total) - nyc_demographics$pop_non_hisp_white)
 
-# Population with and without subway access
+# Only 94,000 New Yorkers who lack subway access live nearby Citi Bike stations;
+# the vast majority (96.2%) do not have bike sharing access. But 95.3% of those
+# with Citi Bike access also live close to the subway.  
 
 subway_service_comparison %>% 
   st_drop_geometry() %>% 
-  filter(subway_service == FALSE) %>% 
-  summarize(pop_without_bike_share = pop_total[1] / sum(pop_total))
+  filter(subway_service == FALSE)
+
+# 1.2 million New York residents have gained access to Citi Bike since 2013, but
+# only 48,700 of them are underprivileged people lacking subway access. The
+# remaining 1.1 million are only slightly more diverse and less affluent than 
+# the original population with bike sharing, with a median household income of
+# $84,900 (compared to $54,700 in the portion of the city without bike sharing
+# access) and a poverty rate of 16.6% (compared with 20.2% in the area without 
+# bike service), half are white (49.9% compared with 26.2% elsewhere), and 44.9%
+# have at least a bachelor’s degree (compared to 19.0% elsewhere).
+
+service_added <- 
+  st_intersect_summarize(
+  CTs,
+  st_intersection(bike_service_growth_comparison["service"], subway_service_areas),
+  group_vars = vars(service, subway_service),
+  population = pop_total,
+  sum_vars = vars(pop_white, education, poverty),
+  mean_vars = vars(med_income)) %>% st_drop_geometry()
+
+service_added %>% 
+  group_by(service) %>% 
+  summarize(pop_total = sum(pop_total))
+
 
 
 
@@ -64,41 +119,7 @@ table_1 <-
 
 ### 3. WHO HAS ACCESS TO CITI BIKE, AND WHO DOESN'T?
 
-## NYC summary demographics
-{nyc_demographics <- get_acs(
-  geography = "county", 
-  variables = c(pop_non_hisp_white = "B03002_003",
-                pop_non_hisp_black = "B03002_004",
-                pop_hisp = "B03002_012",
-                education = "B16010_041",
-                poverty = "B17001_002"),
-  
-  year = 2017, 
-  state = "36",
-  county = c("New York County",
-             "Kings County",
-             "Queens County",
-             "Bronx County",
-             "Richmond County"),
-  summary_var = "B01003_001")
-  
-  names(nyc_demographics) <- c("GEOID", "NAME", "Variable", "Estimate", "MOE", "pop_total",
-                               "pop_total_MOE")
-  
-  nyc_demographics <-
-    nyc_demographics %>%
-    select(-MOE, -pop_total_MOE) %>% 
-    spread(key = Variable, value = Estimate)
-  
-  nyc_demographics <- nyc_demographics %>% filter(pop_total > 100) %>% na.omit() }
 
-nyc_summary_demographics <- c(pop_total = sum(nyc_demographics$pop_total) ,
-                              pop_white = round(sum(nyc_demographics$pop_non_hisp_white)/sum(nyc_demographics$pop_total), 3),
-                              pop_black = round(sum(nyc_demographics$pop_non_hisp_black)/sum(nyc_demographics$pop_total), 3),
-                              pop_hisp = round(sum(nyc_demographics$pop_hisp)/sum(nyc_demographics$pop_total), 3),
-                              education = round(sum(nyc_demographics$education)/sum(nyc_demographics$pop_total), 3),
-                              poverty = round(sum(nyc_demographics$poverty)/sum(nyc_demographics$pop_total), 3)
-)
 
 ## Table 2. Demographic differences in bike sharing access
 
